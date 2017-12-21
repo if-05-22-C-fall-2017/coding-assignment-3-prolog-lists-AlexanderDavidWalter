f3ba16b7fe6cd7098b0e2a6b5a4fec0696195557
AlexanderDavidWalter <32156438+AlexanderDavidWalter@users.noreply.github.com>
2017-12-21 08:43:27 +0100

    Update lists.pl

--- lists.pl
+++ lists.pl
@@ -6,5 +6,5 @@ addElement([H|T],E,[H|L]) :- addElement(T,E,L).
 hasLength([],0).
 hasLength([_|T],N) :- hasLength(T,N2), N is N2 + 1.
 
-remove([E|T],E,T).
+remove([H|T],E,T).
 remove([H|T],E,[H|L]) :- remove(T,E,L).
